1f2d36ae6f2bfd5b2b9061b742cafe9743085f32
Joseph Chu <hotcupofjoe2013@gmail.com>
2026-09-09 14:42:57 -0400

    Just finished the rate limiting using upstash + redis

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/ratelimiter.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
-connectDB();
 
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", noteRoutes);
-
-app.listen(PORT, () => {
-  console.log("Server running on PORT:", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running on PORT:", PORT);
+  });
 });
